951b22c7ddf276b149fcf08c95c9bc887c395273
Lisa Oppermann <lisa@lisabaut.de>
2018-01-06 16:43:15 +0100

    task: wrap main content in container

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Container from './Container'
+
 import './content.scss'
 
 const Content = ({ children }) =>
 	<div className="content">
-		{children}
+		<Container>
+			{children}
+		</Container>
 	</div>;
 
 Content.propTypes = {
