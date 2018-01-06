bfda4ca749104b2177640c830df726b1a3b158ac
Lisa Oppermann <lisa@lisabaut.de>
2018-01-06 10:28:23 +0100

    chore: add pathPrefix for production build
    * as for now the site will live in a subfolder where a direct path is needed for gatsby links to work
    * see docs for pathprefix here => https://www.gatsbyjs.org/docs/path-prefix/
    * see environment variables implementation here => https://stackoverflow.com/questions/47086881/setting-environment-variables-in-gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
+const env = process.env.NODE_ENV || 'development'
+
+const dotenv = require('dotenv')
+dotenv.config({path: `./.env.${env}`})
+
 module.exports = {
+  pathPrefix: process.env.PATH_PREFIX,
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
