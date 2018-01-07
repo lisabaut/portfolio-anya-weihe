b780c223d3d8812b1d14eed807a538a241bddb66
Lisa Oppermann <lisa@lisabaut.de>
2018-01-07 23:12:30 +0100

    refactor: clean-up env set-up in gatsby-config
    * --path-prefix is explicitly set in package json script
    * => therefore no need for env.development

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,28 @@
-const env = process.env.NODE_ENV || 'development'
-
 const dotenv = require('dotenv')
-dotenv.config({path: `./.env.${env}`})
-
-const config = require('./config/site-config')
 const autoprefixer = require('autoprefixer')
+const config = require('./config/site-config')
+dotenv.config()
+
+const pathPrefix = process.env.PATH_PREFIX ? process.env.PATH_PREFIX : '/'
 
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX,
-  siteMetadata: {
+	pathPrefix,
+	siteMetadata: {
 		title: config.siteTitle,
 		metaTags: config.metaTags
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-postcss-sass`,
-      options: {
-        postCssPlugins: [
-          autoprefixer({
-            browsers: ['last 2 versions']
-          })
-        ],
-        precision: 8
-      }
-    }
-  ],
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-plugin-postcss-sass`,
+			options: {
+				postCssPlugins: [
+					autoprefixer({
+						browsers: ['last 2 versions']
+					})
+				],
+				precision: 8
+			}
+		}
+	],
 }
